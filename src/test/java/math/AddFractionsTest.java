32efd9b740a6fd8f4ab1358a0c0dfdd4983f964b
Tom <thomas.spencer@codurance.com>
2020-12-21 12:24:38 +0000

    Add for 7

--- src/test/java/math/AddFractionsTest.java
+++ src/test/java/math/AddFractionsTest.java
@@ -16,4 +16,10 @@ public class AddFractionsTest {
     Fraction plus = new Fraction(0).plus(new Fraction(5));
     assertEquals(plus, new Fraction(5));
   }
+
+  @Test
+  void non_negative_non_zero_operands() {
+    Fraction plus = new Fraction(3).plus(new Fraction(4));
+    assertEquals(plus, new Fraction(7));
+  }
 }
