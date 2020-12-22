f12c965fcd6c82a435c2d481302db06b97409e2b
Tom <thomas.spencer@codurance.com>
2020-12-22 14:42:32 +0000

    Return primes

--- src/main/java/math/ListOfPrimes.java
+++ src/main/java/math/ListOfPrimes.java
@@ -6,8 +6,11 @@ package math;
 // 3. check number in the the list from 2 to sqrt M and divide each number by it
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListOfPrimes {
-  private final int m;
+  private static int m = 0;
 
   public ListOfPrimes(int m) {
     this.m = m;
@@ -35,5 +38,40 @@ public class ListOfPrimes {
       number = number + 2;
     }
     return primes;
+  }
+
+  public static int[] list(int n) {
+    int[] primes = new int[n];
+    primes[0] = 2;
+    int count = 1;
+    int number = 3;
+    boolean numberIsPrime;
+
+    while (count < n){
+      numberIsPrime = true;
+      for (int i = 1; i < (int)(Math.sqrt(number)); i++) {
+        if (number % primes[i] == 0){
+          numberIsPrime = false;
+          break;
+        }
+      }
+      if (numberIsPrime){
+        primes[count] = number;
+        count++;
+      }
+      number = number + 2;
+    }
+    return primes;
+  }
+
+  public static int[] primes(int number) {
+    List<Integer> factors = new ArrayList<>();
+    for(int factor = 2; factor <= number; factor++) {
+      while (number % factor == 0) {
+        factors.add(factor);
+        number = number / factor;
+      }
+    }
+    return factors.stream().mapToInt(n -> n).toArray();
   }
 }
--- src/test/java/math/ListOfPrimesTest.java
+++ src/test/java/math/ListOfPrimesTest.java
@@ -4,11 +4,30 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static math.ListOfPrimes.*;
+
 public class ListOfPrimesTest {
   @Test
   void returns_a_list_of_primes_to_m() {
     int[] list = new ListOfPrimes(9).list();
     int[] result = {2, 3, 5, 7};
+
+    Arrays.equals(result, list);
+  }
+
+  @Test
+  void returns_list_of_primes() {
+    int[] list = list(9);
+    int[] result = {2, 3, 5, 7};
+
+    Arrays.equals(result, list);
+  }
+
+  @Test
+  void returns_prime_factors() {
+    int[] list = primes(20);
+
+    int[] result = {2, 5};
 
     Arrays.equals(result, list);
   }
