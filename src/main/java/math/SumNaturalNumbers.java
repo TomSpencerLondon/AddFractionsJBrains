a5090e9e6ad8edc2dbd77f91caa8c971cf0ed630
Tom <thomas.spencer@codurance.com>
2020-12-23 22:34:42 +0000

    Add formula

--- src/main/java/math/SumNaturalNumbers.java
+++ src/main/java/math/SumNaturalNumbers.java
@@ -1,12 +1,7 @@
 package math;
 
 public class SumNaturalNumbers {
-  public static int sum(int sum){
-    int result = 0;
-    for (int i = 0; i <= sum; i++){
-      result += i;
-    }
-
-    return result;
+  public static int sum(int number){
+    return number * (number + 1) / 2;
   }
 }
